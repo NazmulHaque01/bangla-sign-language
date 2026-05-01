fd894892af07fa244a1fb7ea4ec3a0e55eb3440e
Naiem Racer <mdnazmulhaque0033@gmail.com>
2026-05-02 02:55:17 +0600

    v2: Stable Pure JS Inference with Hybrid Landmarks (153 features) - High Stability Release

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     MODEL_PATH: './models/model_weights.json',
     SCALER_PATH: './data/scaler.json',
     LABELS_PATH: './data/class_labels.json',
-    
+
     // Hand Landmarker settings
     HAND_LANDMARKER: {
         numHands: 2,
@@ -13,25 +13,35 @@ const CONFIG = {
         minTrackingConfidence: 0.5,
         modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task'
     },
-    
+
+    // Pose Landmarker settings (NEW)
+    POSE_LANDMARKER: {
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5,
+        modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task'
+    },
+
     // Inference settings
     INFERENCE: {
         minConfidence: 0.3,
-        updateFrequency: 1  // Update every N frames
+        updateFrequency: 1,  // Update every N frames
+        sustainedDuration: 1000, // ms (1 second)
+        sustainedConfidence: 0.80 // Average confidence required to commit a sign
     },
-    
+
     // UI settings
     UI: {
         maxHistoryItems: 15,
         fpsUpdateInterval: 500  // ms
     },
-    
-    // Feature extraction
+
+    // Feature extraction (UPDATED)
     FEATURES: {
         landmarksPerHand: 21,
         coordsPerLandmark: 3,  // x, y, z
         maxHands: 2,
-        totalFeatures: 126  // 21 * 3 * 2
+        targetPoseIndices: [0, 2, 5, 7, 8, 11, 12, 13, 14], // Nose, Eyes, Ears, Shoulders, Elbows
+        totalFeatures: 153  // 126 (Hands) + 27 (Pose)
     }
 };
 
